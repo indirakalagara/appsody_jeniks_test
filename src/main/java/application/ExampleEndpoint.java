a782f10fdb00c256c6dfa6f070cdd59d7d32ee02
IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
2020-07-07 10:35:20 +0530

    first commit

---
+++ src/main/java/application/ExampleEndpoint.java
@@ -0,0 +1,14 @@
+package application;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleEndpoint {
+
+    @RequestMapping("/starter/hello")
+    public String example() {
+        return "Hello World";
+    }
+}
+
